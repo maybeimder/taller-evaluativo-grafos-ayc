2da1c82e546122f9b57a536b73d50c531eec743d
CarlosElMejor <129225581+Carlinhos2912@users.noreply.github.com>
2025-05-15 14:54:24 -0500

    Update powerIterationMethod.js
    Documentation

--- src/resources/powerIterationMethod.js
+++ src/resources/powerIterationMethod.js
@@ -1,16 +1,19 @@
+//Metodo principal para centralidad por valores propios 
 function powerIterationMethod (initialVector, adjMatrix, iterations) {
     var nextVector = initialVector
+    //Aproximación por número de iteraciones 
     for (let i = 0; i < iterations; i++){
         console.log("Iteración " + i);
-        nextVector = matrixMultiply(nextVector, adjMatrix);
+        nextVector = matrixMultiply(nextVector, adjMatrix); //vector aproximado = (vector*Matriz) / ||vector*Matriz||
         for (let j = 0; j < nextVector[0].length; j++){
-            nextVector[0][j] /= normalize(nextVector)
+            nextVector[0][j] /= normalize(nextVector);
         }
-        console.log("vector propio: " + nextVector + "; m: " + nextVector.length + "; n: " + nextVector[0].length)
+        console.log("vector propio: " + nextVector + "; m: " + nextVector.length + "; n: " + nextVector[0].length);
     }
     return nextVector;
 }
 
+//Metodo para hallar la norma del vector
 function normalize (vector) {
     let sum = 0;
     for (let i = 0; i < vector[0].length; i++){
@@ -20,6 +23,7 @@ function normalize (vector) {
     return Math.sqrt(sum);
 }
 
+//Metodo para calcular la multiplicación de matrices
 function matrixMultiply (A, B) {
     const rowsA = A.length;
     const colsA = A[0].length;
